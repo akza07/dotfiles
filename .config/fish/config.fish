5ab9afb558f6561b6159154cf5cdd3f6c007323c
akzaisme <itsakza07@gmail.com>
2025-05-20 01:42:04 +0530

    Initial Commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,24 @@
+set -g fish_greeting ""
+set -g EDITOR hx
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    zoxide init fish | source
+    alias ls="eza --icons --sort=type"
+    alias l="ls -l --sort=type"
+    alias t="ls --tree"
+    alias code="FLATPAK_ENABLE_SDK_EXT=golang,podman,fish flatpak run com.visualstudio.code"
+end
+# android-studio
+set -gx ANDROID_HOME ~/Android/Sdk
+set -gx PATH $PATH $ANDROID_HOME/emulator $ANDROID_HOME/platform-tools ~/development/flutter/bin
+# pnpm
+set -gx PNPM_HOME "/home/akza/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/akza/.lmstudio/bin
+# End of LM Studio CLI section
+
