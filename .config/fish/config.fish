f8b1cb6d9500341b35b7a5d9de531c49c4dc7a9d
akza07 <itsakza07@gmail.com>
2026-03-03 19:31:20 +0530

    (update): helix fish alias mapping

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,18 @@
 set -g fish_greeting ""
-set -g EDITOR hx
+set -g EDITOR helix
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
     alias ls="eza --icons --sort=type"
     alias l="ls -l --sort=type -a"
     alias t="ls --tree"
+    alias hx="helix"
 end
 # android-studio
 set -gx ANDROID_HOME ~/Android/Sdk
 set -gx PATH $PATH $ANDROID_HOME/emulator $ANDROID_HOME/platform-tools ~/development/flutter/bin
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+
 # pnpm
 set -gx PNPM_HOME "/home/akza/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
